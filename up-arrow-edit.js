4baf686f0c241cf5df133da738bf3261712e923f
Roger Esteve <rogeresteve30@gmail.com>
2024-04-10 00:11:44 +0200

    hide menu if has no edit option

--- up-arrow-edit.js
+++ up-arrow-edit.js
@@ -2,7 +2,11 @@ const MESSAGE_CONTAINER_CLASS = "x3psx0u xwib8y2 xkhd6sd xrmvbpv";
 const HOVER_TARGET = "_amk6 _amlo";
 const ARROW_BUTTON_CLASS = "_ahkm";
 
+let listenerCreated = false;
+
 const handleArrowEdit = () => {
+    if(listenerCreated) return;
+    listenerCreated = true;
     document.addEventListener("keydown", (e) => {
         if (e.key !== "ArrowUp") return;
         const message_input_container = document.querySelector("span.selectable-text[data-lexical-text='true']"); 
@@ -14,7 +18,6 @@ const handleArrowEdit = () => {
 
 const editLastMessage = async () => {
     const mouseOverEvent = createMouseEvent('mouseover');
-    const mouseLeftEvent = createMouseEvent('mouseleft');
     let lastMessage = getLastMessageHoverTarget();
     lastMessage.dispatchEvent(mouseOverEvent);
 
@@ -28,13 +31,19 @@ const editLastMessage = async () => {
     await sleep(10);
 
     const editButton = document.querySelector("div[role='button'][aria-label='Edit']");
-    editButton.click();
+    if (editButton) editButton.click();
+    else hideMenu();
 }
 
 const getLastMessageHoverTarget = () => {
     const message_container = document.getElementsByClassName(MESSAGE_CONTAINER_CLASS)[0];
     const lastMessage = message_container.children[message_container.children.length - 1];
     return lastMessage.getElementsByClassName(HOVER_TARGET)[0];
+}
+
+const hideMenu = () => {
+    const menu = document.querySelector("div._ak4w[role='application']");
+    menu.innerHTML = "";
 }
 
 const createMouseEvent = (t) => {
